c3ddc2b9730cf6b675b4820dce41ab1441abf230
agarciaripalda <agarciaripalda@alumnos.alborfp.com>
2026-03-24 23:35:33 +0100

    Fix: Forzando a Vercel a usar la carpeta nutrition_frontend

--- nutrition_frontend/next.config.js
+++ nutrition_frontend/next.config.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
-=======
 ﻿module.exports = { output: 'export', typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true }, images: { unoptimized: true } };
->>>>>>> 111d767407f43b960a27e350387a5fc6e944a6de
