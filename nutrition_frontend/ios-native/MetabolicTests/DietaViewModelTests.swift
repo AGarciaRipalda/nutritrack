83a21a8b0b040e950dbdf85af250fc6c8c54ed15
AGarciaRipalda <agarciaripalda@alumnos.alborfp.com>
2026-03-27 10:43:37 +0100

    feat: add Dieta screen

---
+++ nutrition_frontend/ios-native/MetabolicTests/DietaViewModelTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import Metabolic
+
+@MainActor
+final class DietaViewModelTests: XCTestCase {
+
+    func testTotalConsumedKcalWhenLoaded() {
+        let vm = DietaViewModel()
+        let meals = [
+            Meal(id: "1", name: "Desayuno", mealType: "desayuno", kcal: 400, proteinG: 20, carbsG: 50, fatG: 10),
+            Meal(id: "2", name: "Almuerzo", mealType: "almuerzo", kcal: 600, proteinG: 40, carbsG: 60, fatG: 15)
+        ]
+        vm.dailyState = .loaded(DietTodayResponse(meals: meals, adherence: nil, stale: nil))
+        XCTAssertEqual(vm.totalConsumedKcal, 1000)
+    }
+
+    func testTotalConsumedKcalWhenIdle() {
+        let vm = DietaViewModel()
+        XCTAssertEqual(vm.totalConsumedKcal, 0)
+    }
+
+    func testTotalConsumedKcalWhenEmpty() {
+        let vm = DietaViewModel()
+        vm.dailyState = .loaded(DietTodayResponse(meals: [], adherence: nil, stale: nil))
+        XCTAssertEqual(vm.totalConsumedKcal, 0)
+    }
+
+    func testDefaultPlanModeIsDaily() {
+        let vm = DietaViewModel()
+        XCTAssertEqual(vm.selectedPlan, DietaViewModel.PlanMode.daily)
+    }
+}
